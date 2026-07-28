db81a01164b31c45ec63f83303092a46d82854d6
Osuji <weezdomosuji@gmail.com>
2026-07-28 16:44:44 +0100

    update 05.sh

--- submission/05.sh
+++ submission/05.sh
@@ -1,12 +1,6 @@
 # How many satoshis did this transaction pay for fee?: b71fb9ab7707407cc7265591e0c0d47d07afede654f91de1f63c0cb522914bcb
 
-TXID="b71fb9ab7707407cc7265591e0c0d47d07afede654f91de1f63c0cb522914bcb"
 
-FEE_SATS=$(bitcoin-cli -signet getrawtransaction "$TXID" 2 | jq '(.fee * 100000000) | round')
-
-if [ -z "$FEE_SATS" ] || [ "$FEE_SATS" = "null" ]; then
-  echo "Error: Could not calculate fee"
-  exit 1
-fi
-
-echo "$FEE_SATS"
+bitcoin-cli -signet getrawtransaction \
+  b71fb9ab7707407cc7265591e0c0d47d07afede654f91de1f63c0cb522914bcb 2 |
+  jq '(.fee * 100000000) | round'
